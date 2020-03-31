eac1ba6f773299973afb45a9ee920849cb5a50aa
Salomón Piña <sarquamon@gmail.com>
2020-03-31 17:46:54 -0600

    Added stuff to app file and routes

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+const indexRoute = require("./api/routes/index");
+const userRoute = require("./api/routes/user");
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use("/", indexRoute);
+app.use("/user", userRoute);
+
+//Error handling
+app.use((req, res, next) => {
+  const error = new Error("Route not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require("http");
 const app = require("./app");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 9000;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}.`);
+  console.log(`🚀 Server started on port: ${PORT}.`);
 });
